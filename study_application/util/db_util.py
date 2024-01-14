cadc2cdce6e0ed82234a4c4d4fcb9a813949abac
kuwaharu-git <kuwaharu6@gmail.com>
2024-01-15 00:10:38 +0900

    db_util.pyの追加
    データベースに接続するようのデコレーター

---
+++ study_application/util/db_util.py
@@ -0,0 +1,33 @@
+import mysql.connector
+# 関数実行前にデータベースに接続、実行後にcloseするでデコレーター
+# エラーが起きた場合はFalse、正常に動作した場合はTrueを返す
+class database_connect:
+    def __init__(self, func):
+        self.func = func
+    def __call__(self, *args, **kwargs):
+        try:
+            cnx = mysql.connector.connect(
+                # データベースにアクセスする際の情報
+                host = "",
+                user = "",
+                password="",
+                database = "" 
+            )
+            cursor = cnx.cursor(dictionary=True)
+            # コネクションが切れた時に再接続する
+            cnx.ping(reconnect=True)
+            try:
+                self.func(cnx, cursor, *args, **kwargs)
+                return True
+            except Exception as e:
+                print("データベースの操作でエラーが起きました。")
+                print(e)
+                return False
+            finally:
+                cursor.close()
+                cnx.close()
+        except Exception as e:
+            print("データベース接続でエラーが起きました。")
+            print(e)
+            return False
+        
