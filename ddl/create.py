b9fbf590621e613e92e7b3e6a238489f71859865
kuwaharu-git <kuwaharu6@gmail.com>
2024-01-21 20:34:03 +0900

    検証用変更

--- ddl/create.py
+++ ddl/create.py
@@ -1,12 +1,12 @@
 import datetime
 from datetime import timedelta, datetime
 from random import randint
-user_id = 2
+user_id = 4
 category_names = ["Python", "web", "MySQL", "English", "Java", "IoT"]
 for _ in range(40):
     category_name = category_names[randint(0, len(category_names) - 1)]
-    study_date = datetime(2024, 1, 1) + timedelta(days=randint(1, 30))
-    study_time = timedelta(hours=randint(0, 4), minutes=randint(0, 59))
+    study_date = datetime(2023, 2, 1) + timedelta(days=randint(4, 30))
+    study_time = timedelta(hours=randint(0, 2), minutes=randint(0, 59))
 
     # SQL文を出力
     sql = f"INSERT INTO study_records (user_id, category_name, study_date, study_time) VALUES ({user_id}, '{category_name}', '{study_date}', '{study_time}');"
